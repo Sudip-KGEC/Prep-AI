a9353a66b6a9bedd21b8f03e3c46cd9505ad7096
Sudip Das <sudipkgec23@gmail.com>
2026-05-13 16:21:55 +0530

    feat: complete initial setup with shadcn/ui, Clerk auth, themes, layouts, and typography

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+     <div>
+       <section className="px-20 py-29">
+        <Button variant="destructive">Click me</Button>
+       </section>
+       
+     </div>
+  );
+}
---
+++ components/Header.jsx
@@ -0,0 +1,44 @@
+import { Show, SignInButton, SignOutButton, SignUpButton, UserButton } from '@clerk/nextjs'
+import React from 'react'
+import { Button } from './ui/button'
+import Link from 'next/link'
+import Image from 'next/image'
+
+const Header = () => {
+    return (
+        <nav className='fixed top-0 inset-x-0 z-50 flex items-center justify-between px-10 py-3 border-b border-white/7 backdrop-blur-xl'>
+          {/* LOGO */}
+            <Link href={"/"}>
+               <Image  src="/logo.png"   priority alt="Prep-AI-Logo" width={100} height={100} className='h-11 w-auto' />
+            </Link>
+
+
+            {/* Redirection logic */}
+
+          {/* Sign In */}
+          <div className='flex items-center gap-3'>
+                <Show when="signed-out">
+
+                    {/* Links  */}
+
+
+                  {/* Credits */}
+
+                <SignInButton >
+                    <Button variant="ghost">
+                        Sign In
+                    </Button>
+                </SignInButton>
+              <SignUpButton  >
+                <Button variant='purple'>Get Started →</Button>
+              </SignUpButton>
+            </Show>
+            <Show when="signed-in">
+              <UserButton />
+            </Show>
+          </div>
+        </nav>
+    )
+}
+
+export default Header
